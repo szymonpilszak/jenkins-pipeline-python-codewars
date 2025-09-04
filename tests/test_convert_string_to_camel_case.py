6a47bfde37e4ab071ee194935fadc92d4ca24a79
szymonpilszak <szymon.pilszak@gmail.com>
2025-09-04 23:59:19 +0200

    Add basic sample of testfile for testing purpose

---
+++ tests/test_convert_string_to_camel_case.py
@@ -0,0 +1,22 @@
+import unittest
+from solutions.Convert_string_to_camel_case import to_camel_case
+
+class TestConvertStringToCamelCase(unittest.TestCase):
+    
+    def test_to_camel_case(self):
+        #Test for basic examples
+        self.assertEqual(to_camel_case("the_stealth_samurai"), "theStealthSamurair")
+        self.assertEqual(to_camel_case("The-Stealth-Samurai"), "TheStealthsamurai")
+        self.assertEqual(to_camel_case("the-Stealth_Samurai"), "theStealthSamurai")
+        
+        #Test for Empty String 
+        self.assertEqual(to_camel_case(""), "")
+        
+        #Test for string without special characters 
+        self.assertEqual(to_camel_case("hello"), "hello")
+        
+        #Test for capitalized string 
+        self.assertEqual(to_camel_case("Hello_world"), "HelloWorld")
+
+if __name__ == "__main__":
+    unittest.main()
