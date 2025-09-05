4e085c2f2e81025d16b41d086fce76089cb25f93
szymonpilszak <szymon.pilszak@gmail.com>
2025-09-05 09:46:08 +0200

    Fix typo in testfile

--- tests/test_convert_string_to_camel_case.py
+++ tests/test_convert_string_to_camel_case.py
@@ -5,7 +5,7 @@ class TestConvertStringToCamelCase(unittest.TestCase):
     
     def test_to_camel_case(self):
         #Test for basic examples
-        self.assertEqual(to_camel_case("the_stealth_samurai"), "theStealthSamurair")
+        self.assertEqual(to_camel_case("the_stealth_samurai"), "theStealthSamurai")
         self.assertEqual(to_camel_case("The-Stealth-Samurai"), "TheStealthsamurai")
         self.assertEqual(to_camel_case("the-Stealth_Samurai"), "theStealthSamurai")
         
